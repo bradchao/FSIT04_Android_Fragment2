5517051c5371476560ebb0dbfc682c56bec09bdb
Brad <brad@brad.tw>
2018-03-18 16:39:25 +0800

    v1

---
+++ app/src/main/java/tw/org/iii/myfragment2/MainActivity.java
@@ -0,0 +1,82 @@
+package tw.org.iii.myfragment2;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager pager;
+    private Page0 page0;
+    private Page1 page1;
+    private Page2 page2;
+    private Page3 page3;
+    private Page4 page4;
+    private FragmentManager fmgr;
+    //private FragmentTransaction tran;
+    private Fragment[] fragments;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.container);
+        page0 = new Page0();
+        page1 = new Page1();
+        page2 = new Page2();
+        page3 = new Page3();
+        page4 = new Page4();
+        fragments = new Fragment[]{page0,page1,page2,page3,page4};
+
+        fmgr = getSupportFragmentManager();
+
+        pager.setAdapter(new MyAdapter(fmgr));
+        pager.setCurrentItem(1);
+        pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0){
+                    pager.setCurrentItem(1);
+                }else if (position == 4){
+                    pager.setCurrentItem(3);
+                }
+            }
+        });
+
+    }
+
+    public void gotoPage1(View view) {
+        pager.setCurrentItem(1);
+    }
+    public void gotoPage2(View view) {
+        pager.setCurrentItem(2);
+    }
+    public void gotoPage3(View view) {
+        pager.setCurrentItem(3);
+    }
+
+    private class MyAdapter extends FragmentStatePagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+    }
+
+}
---
+++ app/src/main/java/tw/org/iii/myfragment2/Page0.java
@@ -0,0 +1,20 @@
+package tw.org.iii.myfragment2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Page0 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page0, container, false);
+    }
+
+}
